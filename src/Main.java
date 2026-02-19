3088fda51a2712fefc594ff90357e037c3d8efae
Fernando Gordillo <gordillo.fernando10@hotmail.com>
2026-02-19 01:00:04 -0600

    First implementation continue with the operation

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("--- |BASIC CALCULATOR JAVA| ---");
+        System.out.println("--- |WELCOME TO BASIC CALCULATOR JAVA| ---");
         System.out.print("\nEnter a Number: ");
         float firstNumber = Float.parseFloat(scanner.nextLine());
 
         char operator;
         boolean isValid = true;
+        do {
+
+        } while ();
         do {
             System.out.printf("Indicate the Operation to Be Performed to %.1f", firstNumber);
             System.out.println("\n(+)\n(-)\n(*)\n(/)");
@@ -40,9 +43,23 @@ public class Main {
         float result = Main.optionSelected(operator, firstNumber, secondNumber);
         System.out.printf("%nResult: %.1f", result);
 
+        char optionContinue;
+        isValid = true;
+        do {
+            System.out.printf("%nContinue doing operations on %.1f Y/n: ", result);
+            optionContinue = scanner.nextLine().charAt(0);
+            isValid = optionContinue == 'Y' || optionContinue == 'y' || optionContinue == 'n' || optionContinue == 'N';
+            System.out.println("*** ENTER A CORRECT OPTION! (Y/n) ***");
+        } while (!isValid);
+
+        if (optionContinue == 'Y' || optionContinue == 'y') {
+            firstNumber = result;
+        }
+
         scanner.close();
     }
 
+    // ### METHODS ###
     public static float optionSelected(char option, float firstNumber, float secondNumber) {
         float result = 0;
         switch (option) {
