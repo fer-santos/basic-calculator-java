1fce9bcd030356e79722ac7c9824be6a812ea256
Fernando Gordillo <gordillo.fernando10@hotmail.com>
2026-02-18 21:08:54 -0600

    First complete basic implementation of the calculator

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,41 @@ public class Main {
         System.out.println("--- |BASIC CALCULATOR JAVA| ---");
         System.out.print("\nEnter a Number: ");
         float firstNumber = Float.parseFloat(scanner.nextLine());
+
         System.out.printf("Indicate the Operation to Be Performed to %.1f", firstNumber);
         System.out.println("\n(+)\n(-)\n(*)\n(/)");
-        char operation = scanner.next().charAt(0);
-        Main.optionSelected(operation);
+        System.out.print("Option: ");
+        char operation = scanner.nextLine().charAt(0);
 
-        System.out.print("Enter a Second Number: ");
+        System.out.printf("%n%.1f %c ", firstNumber, operation);
+        float secondNumber = Float.parseFloat(scanner.nextLine());
+        float result = Main.optionSelected(operation, firstNumber, secondNumber);
+        System.out.printf("Result: %.1f", result);
 
+        scanner.close();
     }
 
-    public static void optionSelected(char option) {
-
+    public static float optionSelected(char option, float firstNumber, float secondNumber) {
+        float result = 0;
+        switch (option) {
+            case '+' -> result = Main.sum(firstNumber, secondNumber);
+            case '-' -> result = Main.res(firstNumber, secondNumber);
+            case '*' -> result = Main.mult(firstNumber, secondNumber);
+            case '/' -> result = Main.div(firstNumber, secondNumber);
+        }
+        return result;
     }
 
-    public static float sum() {
-        return 0;
+    public static float sum(float firstNumber, float secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    public static float res(float firstNumber, float secondNumber) {
+        return firstNumber - secondNumber;
+    }
+    public static float mult(float firstNumber, float secondNumber) {
+        return firstNumber * secondNumber;
+    }
+    public static float div(float firstNumber, float secondNumber) {
+        return firstNumber / secondNumber;
     }
 }
